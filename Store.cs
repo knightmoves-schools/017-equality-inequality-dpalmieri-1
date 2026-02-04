1e4d991581998abfb35d52f8a4614a235cabfc51
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-04 16:19:49 +0000

    I don't understand how these equality things work.

--- Store.cs
+++ Store.cs
@@ -2,4 +2,25 @@
 public class Store
 {
     // Add your code here
+    public static string PriceCheck(double price)
+    {
+        if(price == 1.50)
+        {
+            return "match";
+        } else
+        {
+            return "";
+        }
+    }
+
+    public static string GuessNumber(int guess)
+    {
+        if(guess == 25)
+        {
+            return "";
+        } else
+        {
+            return "not it";
+        }
+    }
 }
